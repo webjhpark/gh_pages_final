418e77c98b25daa376cf513a267c838063b29b6e
webjhpark <web.jhpark@gmail.com>
2024-10-17 11:35:04 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/npx_test/', // 저장소 이름이 my-react-app 이면 /my-react-app/
+  base: '/gh_pages_final/', // 저장소 이름이 my-react-app 이면 /my-react-app/
 });
